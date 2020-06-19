5cb8a8b5357fd6dc090124b1f1425ce75c0d1102
adamoc <oceallaighadam96@gmail.com>
2020-06-19 01:36:16 +0100

    Changed the animation on transition

--- Resources/JS/homepage.js
+++ Resources/JS/homepage.js
@@ -85,7 +85,7 @@ const fadeInHeadingAndLinks = () => {
 const pageTransition = () => {
     var tl = gsap.timeline();
     tl.set('.loading_container img' , {
-        scale: 0.7
+        scale: 0.5
     })
     tl.to('.loading_container', {
         duration: 1.2,
@@ -94,19 +94,20 @@ const pageTransition = () => {
         ease: "circ.out",
     })
     .to('.loading_container img' , {
-        scale : 1,
-        duration: 1
-    } , "-=1.2")
-    .to('.loading_container img' , {
         scale : 0.7,
         duration: 1
-    }, "-=.3")
+    } , "-=1.2")
+    
     .to('.loading_container', {
         duration: 1.2,
         width: "0%",
         right: "0%",
         ease: "circ.out",
     })
+    .to('.loading_container img' , {
+        scale : 0.3,
+        duration: 1.2
+    }, "-=1.3")
 }
 
 // Helper Functions
