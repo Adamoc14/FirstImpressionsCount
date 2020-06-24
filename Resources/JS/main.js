61a2ee9c87e4a5c98c07c9d4889bdd42397fed48
adamoc <oceallaighadam96@gmail.com>
2020-06-24 21:19:57 +0100

    Put my JS into one page

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,4 +1,6 @@
 //Variable Declarations and Function Definitions 
+
+// Homepage Variables 
 let viewBox = ""
 heading_Pos = [0, 0]
 displayState = ""
@@ -9,6 +11,10 @@ logo = $(".Actual_Logo_Svg")
 MorphSVGPlugin.convertToPath("ellipse");
 shapes = Array.from($('.Logo_In_Shapes path'))
 
+// About Page Variables 
+let factsContainer_sm = document.querySelector(".factsContainer_sm")
+
+// Homepage Functions
 const homeInit = () => {
     viewBox = "",
         heading_Pos = [0, 0],
@@ -82,6 +88,129 @@ const fadeInHeadingAndLinks = () => {
     }, "+=.7")
 }
 
+// About Page Functions 
+const aboutInit =() => {
+    factsContainer_sm = document.querySelector(".factsContainer_sm")
+    let head = document.getElementsByTagName('head')[0],
+    link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href= "../../Resources/CSS/about.css"
+    head.appendChild(link);
+}
+
+const face_tl_func = () => {
+    let face_tl = gsap.timeline(),
+        paths = document.querySelectorAll('.My_Face path'),
+        filledYellowElements = ['.Main_Hair_Part', '.Eyeball_2', '.Eyeball_1', '.Nostril_1', '.Nostril_2', '.Tongue_Part'],
+        filledNavyElements = ['.Pupil_2', '.Pupil_1'];
+    face_tl.set(filledNavyElements, { fill: 'unset' }),
+        face_tl.set(filledYellowElements, { fill: 'unset' }),
+        face_tl.fromTo(paths, { drawSVG: "0%" }, { duration: 1, drawSVG: "100% ", stagger: 0.15 })
+    let firstAnimation = gsap.to(filledYellowElements, {
+        duration: 2,
+        ease: "slow",
+        fill: '#F0C368'
+    }, "-=.7"),
+        secondAnimation = gsap.to(filledNavyElements, {
+            duration: 2,
+            ease: "bounce",
+            fill: '#1D373F'
+        }, "-=.7")
+    face_tl.add([firstAnimation, secondAnimation])
+}
+
+
+const scroll_p_tl_func = () => {
+    let scroll_tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.content',
+            start: "top center",
+            end: "+=1000",
+            markers: true,
+            scrub: true
+            // pin: true
+        }
+    })
+    scroll_tl.to('.first', {
+        transform: "rotateX(50deg) rotateZ(331deg) translateX(42px)",
+        duration: .5,
+    }),
+        scroll_tl.to('.flag', {
+            scale: 1
+        }, '-=.1'),
+        scroll_tl.addLabel("first_down")
+    scroll_tl.to('.second', {
+        transform: "rotateX(50deg) rotateZ(331deg) translateX(42px)",
+        duration: 2,
+    }, "first_down-=.1")
+    scroll_tl.addLabel("second_down")
+    scroll_tl.to('.third', {
+        transform: "rotateX(50deg) rotateZ(331deg) translateX(42px)",
+        duration: 2,
+    }, "second_down-=.01")
+}
+
+
+const scroll_skills_tl_func = () => {
+    let scroll_tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.skillsContainer',
+            start: "top center",
+            markers: true,
+
+        }
+    }),
+        barWidth = "",
+        bars = [...document.querySelectorAll('.bar')]
+    bars.map(bar => {
+        barWidth = bar.dataset.width;
+        let barAnimation = gsap.to(bar, {
+            width: barWidth,
+            duration: 1,
+            delay : .2
+        }),
+            percentageAniamtion = gsap.to('.percentage', {
+                scale: 1,
+            })
+        scroll_tl.add([barAnimation, percentageAniamtion]);
+    })
+
+}
+
+const scroll_facts_tl_func = () => {
+    let scroll_tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.factsContainer',
+            start: "top center",
+            // pin: true,
+            scrub: true,
+            end: "+=300",
+            markers: true,
+        }
+    }),
+        facts = [...document.querySelectorAll('.fact')]
+    scroll_tl.to('.factsContainer h2', {
+        scale: 1.5,
+        duration: 1,
+        ease: "slow"
+    })
+    scroll_tl.to(facts, {
+        xPercent: -85 * (facts.length - 1),
+        scrollTrigger: {
+            trigger: ".factsContainer_sm",
+            start: "center center",
+            pin: true,
+            // pinSpacing:false,
+            markers: true,
+            scrub: 1,
+            snap: 1 / (facts.length - 1),
+            // base vertical scrolling on how wide the container is so it feels more natural.
+            end: () => `+=${factsContainer_sm.offsetWidth}`
+        }
+    });
+}
+
+// Transition Functions 
 const pageTransition = () => {
     var tl = gsap.timeline();
     tl.set('.loading_container img', {
@@ -111,7 +240,6 @@ const pageTransition = () => {
 }
 
 // Helper Functions
-
 const delay = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -121,6 +249,11 @@ $(document).ready(() => {
     window.matchMedia("(max-width: 600px)").matches ? logo.attr('viewBox', '-350 -700 1274 1680') : logo.attr('viewBox', '-680 -380 2074 1080')
     var viewbox = window.matchMedia("(max-width: 600px)")
     changeViewBox(viewbox)
+    aboutInit()
+    face_tl_func()
+    scroll_p_tl_func()
+    scroll_skills_tl_func()
+    scroll_facts_tl_func()
 })
 
 hamburger_display_button.onclick = () => {
@@ -180,3 +313,5 @@ barba.init({
 // barba.hooks.enter(() => {
 //     window.scrollTo(0, 0);
 // })
+
+
